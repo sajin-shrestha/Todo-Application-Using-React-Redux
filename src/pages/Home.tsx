5e6a886e3a9d4690c34634d19a4641f717d2c7f7
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-08-01 20:44:15 +0545

    redux-todo-add copleted

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,39 @@
 import { Button } from '@mui/material'
 import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Add } from '../redux/actions/actions'
 import Todo from './Todo'
 
 const Home = () => {
   const [data, setData] = useState('')
+  const dispatch = useDispatch()
+
+  const addData = () => {
+    if (data.trim() !== '') {
+      dispatch(Add(data))
+      setData('') // Clear the input field after adding data
+    }
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      addData()
+    }
+  }
 
   return (
     <>
       <div className="container">
         <section className="mt-3 text-center">
           <h2>Enter Your Task</h2>
-
           <div className="todo col-lg-5 mx-auto d-flex justify-content-between align-items-center">
             <input
               type="text"
               name="task"
               value={data}
               className="form-control custom-input"
-              onChange={(e) => {
-                setData(e.target.value)
-              }}
+              onChange={(e) => setData(e.target.value)}
+              onKeyDown={handleKeyDown}
             />
             <Button
               sx={{
@@ -33,11 +47,11 @@ const Home = () => {
                 },
               }}
               className="mx-2"
+              onClick={addData}
             >
               Add
             </Button>
           </div>
-
           <Todo />
         </section>
       </div>
